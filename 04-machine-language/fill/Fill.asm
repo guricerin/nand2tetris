b2c2352509caea33af177631e261212eae1e9585
guricerin <chanbo1e9@gmail.com>
2020-12-19 18:23:12 +0900

    fill.asm

--- 04-machine-language/fill/Fill.asm
+++ 04-machine-language/fill/Fill.asm
@@ -6,9 +6,69 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+    // lim = 24575 (screen's memory map end)
+    @24575
+    D=A
+    @lim
+    M=D
+
+(LOOP)
+    // addr = 16384 (screen's base address)
+    @SCREEN
+    D=A
+    @addr
+    M=D
+
+    // if KBD = 0 then goto CLEAR
+    // else goto FILL
+    @KBD
+    D=M
+    @CLEAR
+    D;JEQ
+
+(FILL)
+    // if addr - lim > 0 then goto LOOP
+    @addr
+    D=M
+    @lim
+    D=D-M // addr - lim
+    @LOOP
+    D;JGT
+
+    // RAM[addr] = [1; 16]
+    @addr
+    A=M
+    M=-1
+
+    // addr++
+    @addr
+    M=M+1
+
+    @FILL
+    0;JMP
+
+(CLEAR)
+    // if addr - lim > 0 then goto LOOP
+    @addr
+    D=M
+    @lim
+    D=D-M // addr - lim
+    @LOOP
+    D;JGT
+
+    // RAM[addr] = [0; 16]
+    @addr
+    A=M
+    M=0
+
+    // addr++
+    @addr
+    M=M+1
+
+    @CLEAR
+    0;JMP
