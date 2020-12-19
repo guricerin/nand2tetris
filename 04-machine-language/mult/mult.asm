5eb4d21e8ca3bb28a0d4669ec84b44bb43f163fd
guricerin <chanbo1e9@gmail.com>
2020-12-19 13:22:13 +0900

    mult

--- 04-machine-language/mult/mult.asm
+++ 04-machine-language/mult/mult.asm
@@ -6,4 +6,25 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @i
+    M=1 // i = 1
+    @R2
+    M=0 // R2 = 0
+(LOOP)
+    @i
+    D=M // D = i
+    @R1
+    D=D-M // D = i - R1
+    @END
+    D;JGT // if (i - R1) > 0 then goto END
+    @R0
+    D=M // D = R0
+    @R2
+    M=M+D // R2 = R2 + R0
+    @i
+    M=M+1 // i = i + 1
+    @LOOP
+    0;JMP // goto LOOP
+(END)
+    @END
+    0;JMP // exit
