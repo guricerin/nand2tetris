98069c485c2af03c476dc433b7db5ae25f7c89a7
guricerin <chanbo1e9@gmail.com>
2020-12-23 10:49:21 +0900

    parse mem_access

--- vm-translator/src/lib.rs
+++ vm-translator/src/lib.rs
@@ -1,10 +1,2 @@
 mod code;
 mod parser;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
--- vm-translator/src/parser/mod.rs
+++ vm-translator/src/parser/mod.rs
@@ -5,11 +5,11 @@ mod mem_access;
 mod segment;
 mod stack;
 
-// use anyhow::{anyhow, Context, Result};
 use arithmetic::*;
 use flow::*;
 use func::*;
 use mem_access::*;
+use segment::*;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -20,6 +20,8 @@ pub enum ParseError {
     LackTokens(String),
     #[error("redundant tokens: {0}")]
     RedundantToken(String),
+    #[error(transparent)]
+    ParseNum(#[from] std::num::ParseIntError),
     #[error("end of file")]
     Eof,
 }
@@ -50,6 +52,12 @@ impl Command {
     pub fn arithmetic(arith: Arithmetic) -> Self {
         Self::Arithmetic(arith)
     }
+    pub fn push(seg: Segment, n: u16) -> Self {
+        Self::MemAccess(MemAccess::Push(seg, n))
+    }
+    pub fn pop(seg: Segment, n: u16) -> Self {
+        Self::MemAccess(MemAccess::Pop(seg, n))
+    }
     // pub fn mem_access(seg: Segment, index: u16) -> Self {
     //     let m = MemAccess(seg, index);
     //     Self::MemAccess(m)
@@ -62,9 +70,9 @@ pub struct Parser {
 }
 
 macro_rules! parse_arithmetic {
-    ($self:ident, $tokens:expr, $cmd:expr) => {{
+    ($self:ident, $tokens:expr, $arith:expr) => {{
         let _ = Parser::check_tokens_num($tokens, 1)?;
-        let cmd = Command::Arithmetic($cmd);
+        let cmd = Command::Arithmetic($arith);
         Ok(cmd)
     }};
 }
@@ -92,7 +100,7 @@ impl Parser {
     fn parse_line(&self, tokens: &Vec<&str>) -> Result<Command, ParseError> {
         match tokens[0] {
             // arith
-            "add" => self.parse_add(tokens),
+            "add" => parse_arithmetic!(self, tokens, Arithmetic::Add),
             "sub" => parse_arithmetic!(self, tokens, Arithmetic::Sub),
             "neg" => parse_arithmetic!(self, tokens, Arithmetic::Neg),
             "eq" => parse_arithmetic!(self, tokens, Arithmetic::Eq),
@@ -102,17 +110,8 @@ impl Parser {
             "or" => parse_arithmetic!(self, tokens, Arithmetic::Or),
             "not" => parse_arithmetic!(self, tokens, Arithmetic::Not),
             // memory access
-            "push" => todo!(),
-            "pop" => todo!(),
-            // segment
-            "argument" => todo!(),
-            "local" => todo!(),
-            "static" => todo!(),
-            "constant" => todo!(),
-            "this" => todo!(),
-            "that" => todo!(),
-            "pointer" => todo!(),
-            "temp" => todo!(),
+            "push" => Parser::parse_push(tokens),
+            "pop" => Parser::parse_pop(tokens),
             // program flow
             "label" => todo!(),
             "goto" => todo!(),
@@ -134,10 +133,42 @@ impl Parser {
             Ok(())
         }
     }
-    fn parse_add(&self, tokens: &Vec<&str>) -> Result<Command, ParseError> {
-        let _ = Parser::check_tokens_num(tokens, 1)?;
-        let cmd = Command::arithmetic(Arithmetic::Add);
+
+    fn parse_push(tokens: &Vec<&str>) -> Result<Command, ParseError> {
+        let _ = Parser::check_tokens_num(tokens, 3)?;
+        let seg = Parser::parse_segment(tokens[1])?;
+        let n = Parser::parse_num(tokens[2])?;
+        let cmd = Command::push(seg, n);
         Ok(cmd)
+    }
+
+    fn parse_pop(tokens: &Vec<&str>) -> Result<Command, ParseError> {
+        let _ = Parser::check_tokens_num(tokens, 3)?;
+        let seg = Parser::parse_segment(tokens[1])?;
+        let n = Parser::parse_num(tokens[2])?;
+        let cmd = Command::pop(seg, n);
+        Ok(cmd)
+    }
+
+    fn parse_segment(tok: &str) -> Result<Segment, ParseError> {
+        let seg = match tok {
+            // segment
+            "argument" => Segment::Arg,
+            "local" => Segment::Local,
+            "static" => Segment::Static,
+            "constant" => Segment::Constant,
+            "this" => Segment::This,
+            "that" => Segment::That,
+            "pointer" => Segment::Pointer,
+            "temp" => Segment::Temp,
+            _ => return Err(ParseError::unexpected_token(tok)),
+        };
+        Ok(seg)
+    }
+
+    fn parse_num(tok: &str) -> Result<u16, ParseError> {
+        let n: u16 = tok.parse()?;
+        Ok(n)
     }
 }
 
@@ -147,6 +178,7 @@ mod tests {
 
     #[test]
     fn test_parse_arith() {
+        let parser = Parser::new("test");
         let input = r###"
         add
         sub
@@ -158,7 +190,6 @@ mod tests {
         or
         not
         "###;
-        let parser = Parser::new("test");
         let actual = parser.parse(input).unwrap();
         let expect = vec![
             Command::Arithmetic(Arithmetic::Add),
@@ -172,5 +203,85 @@ mod tests {
             Command::Arithmetic(Arithmetic::Not),
         ];
         assert_eq!(actual, expect);
+    }
+
+    #[test]
+    fn test_parse_arith_err() {
+        let parser = Parser::new("test");
+        let input = "add local";
+        let actual = parser.parse(input);
+        assert!(actual.is_err(), "redundant token");
+
+        let input = "sub not";
+        let actual = parser.parse(input);
+        assert!(actual.is_err(), "redundant token");
+
+        let input = "eq 123";
+        let actual = parser.parse(input);
+        assert!(actual.is_err(), "redundant token");
+
+        let input = "gt 123 argument";
+        let actual = parser.parse(input);
+        assert!(actual.is_err(), "redundant token");
+    }
+
+    #[test]
+    fn test_parse_mem_access() {
+        let parser = Parser::new("test");
+        let input = r###"
+        push argument 0
+        pop argument 1
+        push local 2
+        pop local 3
+        push static 4
+        pop static 5
+        push constant 6
+        pop constant 7
+        push this 8
+        pop this 9
+        push that 10
+        pop that 11
+        push pointer 12
+        pop pointer 13
+        push temp 14
+        pop temp 15
+        "###;
+        let actual = parser.parse(input).unwrap();
+        let expect = vec![
+            Command::push(Segment::Arg, 0),
+            Command::pop(Segment::Arg, 1),
+            Command::push(Segment::Local, 2),
+            Command::pop(Segment::Local, 3),
+            Command::push(Segment::Static, 4),
+            Command::pop(Segment::Static, 5),
+            Command::push(Segment::Constant, 6),
+            Command::pop(Segment::Constant, 7),
+            Command::push(Segment::This, 8),
+            Command::pop(Segment::This, 9),
+            Command::push(Segment::That, 10),
+            Command::pop(Segment::That, 11),
+            Command::push(Segment::Pointer, 12),
+            Command::pop(Segment::Pointer, 13),
+            Command::push(Segment::Temp, 14),
+            Command::pop(Segment::Temp, 15),
+        ];
+        assert_eq!(actual, expect);
+    }
+
+    #[test]
+    fn test_parse_mem_access_err() {
+        let parser = Parser::new("test");
+        let input = "push";
+        let actual = parser.parse(input);
+        assert!(actual.is_err(), "lack token");
+        let input = "pop 100";
+        let actual = parser.parse(input);
+        assert!(actual.is_err(), "unexpected token");
+        let input = "pop local local";
+        let actual = parser.parse(input);
+        assert!(actual.is_err(), "unexpected token");
+        let input = "push local 100 local";
+        let actual = parser.parse(input);
+        assert!(actual.is_err(), "redundant token");
     }
 }
